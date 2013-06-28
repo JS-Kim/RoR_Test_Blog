242baf9c77d591b2f77d67fffbcf245686143458
JS-Kim <hockeygirl92@gmail.com>
2013-06-28 12:10:10 -0400

    just added a comment for testing git-usage:

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title, :tags_attributes
   has_many :comments
   has_many :tags
-
+#add comments for testing git
   accepts_nested_attributes_for :tags, :allow_destroy => true, :reject_if => proc { |attrs| attrs.all? {|k,v|v.blank? }}
 end
