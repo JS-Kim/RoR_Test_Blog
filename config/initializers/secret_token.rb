8ea493fbd62dea1d7042f4999d9420c2dcb15f24
JS-Kim <hockeygirl92@gmail.com>
2013-06-28 11:51:38 -0400

    * Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '13481b0bc8b22c15bbf2b8b259b3fdc6cac005cc3b4ed537b39631c691ba5b62d508251e09f4386ab35b186d57934513cf54ab8b0ebbb243bd88563e0b8a2b68'
